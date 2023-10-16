7291e83c4137fd3073e49d42638116b1db43ebe8
Stefan Irrgang <122272798+StefanIrrgang@users.noreply.github.com>
2023-10-16 10:53:38 +0200

    channel chat chat area

--- src/app/chat-area/chat-area.component.ts
+++ src/app/chat-area/chat-area.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chat-area',
   templateUrl: './chat-area.component.html',
   styleUrls: ['./chat-area.component.scss']
 })
-export class ChatAreaComponent {
+export class ChatAreaComponent implements OnInit {
+
+  formattedDate!: string;
+
+  ngOnInit() {
+    this.formatiereDatum();
+  }
+
+  private formatiereDatum() {
+    const tage = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+    const monate = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
+
+    const aktuellesDatum = new Date();
+    const wochentag = tage[aktuellesDatum.getDay()];
+    const tag = aktuellesDatum.getDate();
+    const monat = monate[aktuellesDatum.getMonth()];
+
+    this.formattedDate = `${wochentag}, ${tag}. ${monat}`;
+  }
 
 }
