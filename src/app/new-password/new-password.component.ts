ea0a2b58042d109be1136a07238fca380a57751e
Jonas Wemmers <jonas.wemmers@freenet.de>
2023-10-19 19:28:26 +0200

    PW Reset E-Mail

--- src/app/new-password/new-password.component.ts
+++ src/app/new-password/new-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Auth, confirmPasswordReset } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-new-password',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-password.component.scss']
 })
 export class NewPasswordComponent {
+  newPassword: string = '';
+  confirmPassword: string = ''; // Fügen Sie die confirmPassword-Variable hinzu
+  oobCode: string = ''; // Hier sollten Sie den OOB-Code aus dem Link in der E-Mail einfügen.
 
+  constructor(private auth: Auth) { }
+
+  async changePassword() {
+    if (this.oobCode && this.newPassword && this.confirmPassword) { // Überprüfen Sie die confirmPassword-Variable
+      if (this.newPassword === this.confirmPassword) {
+        try {
+          await confirmPasswordReset(this.auth, this.oobCode, this.newPassword);
+
+          console.log('Passwort wurde erfolgreich geändert und in der Datenbank aktualisiert.');
+        } catch (error) {
+          console.error('Fehler bei der Passwortänderung:', error);
+        }
+      } else {
+        console.error('Die neuen Passwörter stimmen nicht überein.');
+      }
+    } else {
+      console.error('Ungültige Daten für die Passwortänderung.');
+    }
+  }
 }
--- src/app/password-reset/password-reset.component.ts
+++ src/app/password-reset/password-reset.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Auth, sendPasswordResetEmail } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-password-reset',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./password-reset.component.scss']
 })
 export class PasswordResetComponent {
+  email: string = '';
 
+  constructor(private auth: Auth) { }
+
+  resetPassword() {
+    sendPasswordResetEmail(this.auth, this.email)
+      .then(() => {
+        // Erfolgreiche Nachricht an den Benutzer anzeigen
+        console.log('Passwort-Zurücksetzungs-E-Mail wurde gesendet.');
+      })
+      .catch((error: any) => {
+        console.error('Ein Fehler ist aufgetreten:', error);
+      });
+  }
 }
